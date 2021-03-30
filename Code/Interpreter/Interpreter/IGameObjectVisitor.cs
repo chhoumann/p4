cb1dbef184d4cc9dd3d9d07759b20947bf8fde37
Ivikhostrup <56341364+Ivikhostrup@users.noreply.github.com>
2021-03-30 11:34:48 +0200

    Initial thoughts

---
+++ Code/Interpreter/Interpreter/IGameObjectVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Interpreter
+{
+    public interface IGameObjectVisitor
+    {
+        void Visit(GameObject go);
+        void Visit(GameObjectType go_Type);
+        void Visit(GameObjectContent go_Content);
+
+    }
+    
+    public interface IGameObject 
+}
+
