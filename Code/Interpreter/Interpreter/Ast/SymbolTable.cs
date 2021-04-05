76635975afda673606c657bf9d4841b8799ee2e3
Pattrigue <patrick@gamehouse.dk>
2021-04-05 10:19:28 +0200

    Create SymbolTable.cs

---
+++ Code/Interpreter/Interpreter/Ast/SymbolTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Interpreter.Ast.Nodes.ExpressionNodes;
+
+namespace Interpreter.Ast
+{
+    public sealed class SymbolTable
+    {
+        public static SymbolTable Instance => Singleton.Value;
+        
+        public Dictionary<string, Value> Values { get; set; } = new Dictionary<string, Value>();
+        public List<SymbolTable> SymbolTables { get; set; } = new List<SymbolTable>();
+
+        private static readonly Lazy<SymbolTable> Singleton = new(() => new SymbolTable());
+        
+        
+    }
+}
