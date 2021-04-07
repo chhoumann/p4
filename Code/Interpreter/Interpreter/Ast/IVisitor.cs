e29654738593ff3045e829a17fde30258898b396
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-04-07 15:23:01 +0200

    Visitor reordering

--- Code/Interpreter/Interpreter/Ast/IVisitor.cs
+++ Code/Interpreter/Interpreter/Ast/IVisitor.cs
@@ -7,30 +7,30 @@ namespace Interpreter.Ast
 {
     public interface IVisitor
     {
-        void Visit(Array array);
+        void Visit(GameObject gameObject);
+        void Visit(MapType mapType);
+        void Visit(PatternType patternType);
+        void Visit(OnScreenEnteredType onScreenEnteredType);
+        void Visit(EntityType entityType);
+        void Visit(DataType dataType);
+        void Visit(EntitiesType entitiesType);
+        void Visit(ExitsType exitsType);
+        void Visit(GameObjectContent gameObjectContent);
+        void Visit(StatementBlock statementBlock);
         void Visit(FactorExpression factorExpression);
         void Visit(FactorOperation factorOperation);
-        void Visit(FloatValue floatValue);
-        void Visit(IdentifierValue identifierValue);
-        void Visit(IntValue intValue);
-        void Visit(MemberAccess memberAccess);
         void Visit(SumExpression sumExpression);
         void Visit(SumOperation sumOperation);
         void Visit(TerminalExpression terminalExpression);
-        void Visit(EntityType entityType);
-        void Visit(GameObject gameObject);
-        void Visit(GameObjectContent gameObjectContent);
         void Visit(MovePatternType movePatternType);
         void Visit(ScreenType gameObjectContent);
         void Visit(IfStatement ifStatement);
         void Visit(RepeatNode repeatNode);
         void Visit(StatementExpression statementExpression);
-        void Visit(DataType dataType);
-        void Visit(EntitiesType entitiesType);
-        void Visit(ExitsType exitsType);
-        void Visit(MapType mapType);
-        void Visit(OnScreenEnteredType onScreenEnteredType);
-        void Visit(PatternType patternType);
-        void Visit(StatementBlock statementBlock);
+        void Visit(MemberAccess memberAccess);
+        void Visit(FloatValue floatValue);
+        void Visit(IdentifierValue identifierValue);
+        void Visit(IntValue intValue);
+        void Visit(Array array);
     }
 }
