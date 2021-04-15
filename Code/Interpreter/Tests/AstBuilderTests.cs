0bfab17166af419f05e50511cb66eb7b21c9c33e
Pattrigue <patrick@gamehouse.dk>
2021-04-15 11:10:31 +0200

    Create AstBuilderTests.cs

---
+++ Code/Interpreter/Tests/AstBuilderTests.cs
@@ -0,0 +1,29 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Interpreter.Ast;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public sealed class AstBuilderTests
+    {
+        private IParseTree parseTree;
+        private const string TestCodePath = "./dazel_test_code.txt";
+
+        [SetUp]
+        public void Setup()
+        {
+            ICharStream stream = CharStreams.fromPath(TestCodePath);
+            ITokenSource lexer = new DazelLexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            parseTree = new DazelParser(tokens) {BuildParseTree = true}.start();
+        }
+        
+        [Test]
+        public void fml()
+        {
+            new AstBuilder().BuildAst(parseTree);
+        }
+    }
+}
