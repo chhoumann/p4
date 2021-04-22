ac7c69dba08ae150dac30fb3150b431d5f0b1a00
Ivikhostrup <56341364+Ivikhostrup@users.noreply.github.com>
2021-04-22 10:29:14 +0200

    WIP

---
+++ Code/Interpreter/Interpreter/SemanticAnalysis/SemanticAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Interpreter.SemanticAnalysis
+{
+    public class SemanticAnalysis
+    {
+        private Stack<SymbolTable> environmentStack;
+        
+        
+    }
+
+    public sealed class SymbolTable
+    {
+        public Dictionary<string, string> symbols { get; } = new();
+
+        public string RetrieveSymbol()
+        {
+            return symbols.
+        }
+    }
+}
