56a027a47aae570ba2e1279471e5492c52856073
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-05-03 10:26:38 +0200

    Changed how we handle test Dazel code. First semantics test done.

--- Code/Interpreter/Tests/Semantics/TypeCheckTests.cs
+++ Code/Interpreter/Tests/Semantics/TypeCheckTests.cs
@@ -11,36 +11,48 @@ namespace Tests.Semantics
     internal class TypeCheckTests
     {
         private IParseTree parseTree;
-        private const string TestCodePath = "./dazel_test_code.txt";
+
+        private const string TestCode =
+            "Screen SampleScreen1" + // Test GameObject
+            "{" +
+            "   Map" + // Test GameObjectContent & GameObjectContentType
+            "   {" +
+            "       Size(30, 24);" + // Test function invocation
+            "       SomeVar1 = 2.0;" + // Test assignment & floats
+            "       { " + // Test statement block
+            "           SomeVar2 = 3 + 3 / 3;" + // Test expressions & integers
+            "           x = SomeVar2;" + // Test identifier values
+            "       }" +
+            "       let = SampleScreen1.Exits.exit1;" + // Test member access 
+            "       arr = [1, 2, 3];" + // Test arrays
+            "   }" +
+            "" +
+            "   Entities" +
+            "   {" +
+            "       SpawnEntity(Skeleton1, [4, 5]);" +
+            "   }" +
+            "}";
 
         [SetUp]
         public void Setup()
         {
-            ICharStream stream = CharStreams.fromPath(TestCodePath);
+            ICharStream stream = CharStreams.fromString(TestCode);
             ITokenSource lexer = new DazelLexer(stream);
             ITokenStream tokens = new CommonTokenStream(lexer);
             parseTree = new DazelParser(tokens) {BuildParseTree = true}.start();
         }
 
         [Test]
-        public void TypeCheck_ExpectPass()
+        public void TypeCheck_Visit_AllScopesAccountedFor()
         {
             var ast = new AstBuilder().BuildAst(parseTree);
             var tc = new TypeChecker(ast);
+            
+            
             tc.Visit(ast.Root.GameObjects["SampleScreen1"]);
+            
+            // We know that, if the stack height is 0, then the scopes have all been popped off the stack.
             Assert.That(tc.EnvironmentStack.Count == 0);
         }
-        
-        [Test]
-        public void TypeCheck_ExpectaPass()
-        {
-            var ast = new AstBuilder().BuildAst(parseTree);
-            var tc = new TypeChecker(ast);
-            tc.Visit(ast.Root.GameObjects["SampleScreen1"]);
-            Assert.That(tc.EnvironmentStack.Count == 0);
-        }
-        
-        
-        
     }
 }
