8d36dc50ac33ba16ca5829d6d7bc1ce85dbd5cac
Pattrigue <patrick@gamehouse.dk>
2021-05-10 12:01:56 +0200

    Update TileModel.cs

--- Code/Adventure-Map-Generator/Assets/Scripts/IntermediateModels/TileModel.cs
+++ Code/Adventure-Map-Generator/Assets/Scripts/IntermediateModels/TileModel.cs
@@ -2,9 +2,16 @@ namespace Dazel.IntermediateModels
 {
     public sealed class TileModel
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public int X { get; }
+        public int Y { get; }
 
-        public string GraphicName { get; set; }
+        public string GraphicName { get; }
+
+        public TileModel(int x, int y, string graphicName)
+        {
+            X = x;
+            Y = y;
+            GraphicName = graphicName;
+        }
     }
 }
