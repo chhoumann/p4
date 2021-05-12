4abdae52fef3997c989e8425b49cadefb1fdc4ba
Pattrigue <patrick@gamehouse.dk>
2021-05-12 18:12:43 +0200

    WIP ScreenExits

--- Code/Adventure-Map-Generator/Assets/Scripts/Interpreter/StandardLibrary/Functions/ExitsFunctions/ScreenExitFunction.cs
+++ Code/Adventure-Map-Generator/Assets/Scripts/Interpreter/StandardLibrary/Functions/ExitsFunctions/ScreenExitFunction.cs
@@ -1,17 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dazel.Game;
+using Dazel.Interpreter.Ast;
 using Dazel.Interpreter.Ast.Nodes.ExpressionNodes.Values;
+using Dazel.Interpreter.Ast.Nodes.GameObjectNodes;
 using Dazel.Interpreter.SemanticAnalysis;
 
 namespace Dazel.Interpreter.StandardLibrary.Functions.ExitsFunctions
 {
-    public class ScreenExitFunction : Function
+    public sealed class ScreenExitFunction : Function
     {
         public override int NumArguments => 2;
+        
         public ScreenExitFunction() : base(SymbolType.Void) { }
+
+        public Direction ExitDirection { get; private set; }
+        public GameObjectNode ConnectedScreen { get; private set; }
+
+        private string connectedScreenName;
 
         public override ValueNode GetReturnType(List<ValueNode> parameters)
         {
-            // TODO: Should return some exit type so we can assign to exits
+            if (parameters[0] is IdentifierValueNode dirId && parameters[1] is IdentifierValueNode screenId
+                && Enum.TryParse(dirId.Value, false, out Direction exitDirection))
+            {
+                // TODO: Should return some exit type so we can assign to exits 
+                ExitDirection = exitDirection;
+                connectedScreenName = screenId.Value;
+            }
+
+            throw InvalidArgumentsException(parameters);
+        }
+
+        public override ValueNode Setup(List<ValueNode> parameters, AbstractSyntaxTree ast)
+        {
+            if (ast.TryRetrieveGameObject(connectedScreenName, out GameObjectNode screen))
+            {
+                ConnectedScreen = screen;
+            }
+            
             return null;
         }
     }
