4ab9ff4c6a5d589c7d3daf02fe52e7a87cc6d05e
Pattrigue <patrick@gamehouse.dk>
2021-05-12 14:59:21 +0200

    Update Setup.cs

--- Code/Adventure-Map-Generator/Assets/Scripts/Game/Setup.cs
+++ Code/Adventure-Map-Generator/Assets/Scripts/Game/Setup.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using Dazel.IntermediateModels;
 using Dazel.Interpreter;
 using UnityEngine;
 using UnityEngine.SceneManagement;
