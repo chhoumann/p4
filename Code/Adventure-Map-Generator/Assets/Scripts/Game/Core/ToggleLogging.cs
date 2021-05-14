b033c982d5916c61239b8ecef38a8ab5ba2a12f4
Pattrigue <patrick@gamehouse.dk>
2021-05-15 00:11:44 +0200

    Update ToggleLogging.cs

--- Code/Adventure-Map-Generator/Assets/Scripts/Game/Core/ToggleLogging.cs
+++ Code/Adventure-Map-Generator/Assets/Scripts/Game/Core/ToggleLogging.cs
@@ -5,16 +5,30 @@ namespace Dazel.Game.Core
 {
     public sealed class ToggleLogging : MonoBehaviour
     {
+        private const string PlayerPrefsKey = "LoggingEnabled";
+        
         private void Awake()
         {
             Toggle toggle = GetComponent<Toggle>();
-            toggle.SetIsOnWithoutNotify(GameManager.Instance.LoggingEnabled);
+            
+            if (PlayerPrefs.HasKey(PlayerPrefsKey))
+            {
+                bool isOn = PlayerPrefs.GetInt(PlayerPrefsKey) == 1;
+                OnToggleChanged(isOn);
+                toggle.SetIsOnWithoutNotify(isOn);
+            }
+            else
+            {
+                toggle.SetIsOnWithoutNotify(GameManager.Instance.LoggingEnabled);
+            }
+            
             toggle.onValueChanged.AddListener(OnToggleChanged);
         }
 
         private static void OnToggleChanged(bool isOn)
         {
             GameManager.Instance.LoggingEnabled = isOn;
+            PlayerPrefs.SetInt(PlayerPrefsKey, isOn ? 1 : 0);
         }
     }
 }
