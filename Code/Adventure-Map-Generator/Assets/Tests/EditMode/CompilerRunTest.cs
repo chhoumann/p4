1e5598b2b152966dff59a28d4ce26aa720268f19
Christian Bager Bach Houmann <christian@bagerbach.com>
2021-05-26 12:13:46 +0100

    Integration testing section (#82)
    * Update CompilerRunTest.cs
    
    * Integration testing section
    
    * wip
    
    * Update discussion.tex
    
    * update test and discussion
    
    Co-authored-by: Ivikhostrup <56341364+Ivikhostrup@users.noreply.github.com>

--- Code/Adventure-Map-Generator/Assets/Tests/EditMode/CompilerRunTest.cs
+++ Code/Adventure-Map-Generator/Assets/Tests/EditMode/CompilerRunTest.cs
@@ -7,23 +7,70 @@ namespace Tests.EditMode
     [TestFixture]
     public class CompilerRunTest : DazelTestBase
     {
-        private const string TestCode1_1 =
-            "Screen SampleScreen1" +
-            "{" +
-            "   Exits" +
-            "   {" +
-            "       s1exit1 = Exit([0, 0], SampleScreen2.Exits.s2exit1);" + 
-            "   }" +
-            "}";
-        
-        private const string TestCode1_2 = 
-            "Screen SampleScreen2" +
-            "{" +
-            "   Exits" +
-            "   {" +
-            "       s2exit1 = Exit([0, 0], SampleScreen1.Exits.s1exit1);" + 
-            "   }" +
-            "}";
+	    private const string TestCode1_1 = @"Screen SampleScreen1
+		{
+			Map 
+			{
+				Size(80, 24);
+				// comment
+		        SomeVar1 = 2;
+			    {
+			        SomeVar2 = 3 + 3;
+			        {
+						SomeVar3 = 2 + 2;
+			        }
+			    }
+			    
+			    x = 4;
+				Print(x);
+				
+				z = SampleScreen2.Exits.Exit1;
+	    	    			    
+				Walls(""Stone.png""); 
+				Floor(""Grass"");
+				
+		        {
+				    Line([2, 2], [2, 4], ""Cliff.png"");
+		        }
+
+				Square(position, size, ""Cliff.png"");
+				Square([8, 8], 4, ""Cliff.png"");
+			}
+
+			Entities 
+			{
+				SpawnEntity(""Skeleton1"", [4, 5]);
+				SpawnEntity(""Skeleton1"", [4, 6]);
+			}
+			
+			Exits 
+			{
+				Exit1 = Exit([4, 0], SampleScreen2.Exits.Exit1);
+				Print(""HasExit"");
+				Print(Exit1);
+
+				ScreenExit(Down, SampleScreen2);
+				ScreenExit(Left, SampleScreen2);
+				SomeVar = 3 + 3 + (3 + 3 + 3 + 3 * 3 * 2 * 1 * 5 * 1 - 3 + 8419);
+				SomeVar = SomeVar + 3;
+			}
+		}";
+
+	    private const string TestCode1_2 = @"Screen SampleScreen2
+		{
+			Map 
+			{
+				Size(20, 20);
+		        
+				Walls(""Grass.png""); 
+				Floor(""Stone.png"");
+			}
+			
+			Exits 
+			{
+				Exit1 = Exit([1,1], SampleScreen1.Exits.Exit1);
+			}
+		}";
 
         [Test]
         public void DazelCompiler_TryRun_SucceedOnValidSourceCode()
@@ -52,7 +99,7 @@ namespace Tests.EditMode
             "}";
 
         [Test]
-        public void DazelCompiler_TryRun_ThrowOnValidSourceCode()
+        public void DazelCompiler_TryRun_ThrowOnInvalidSourceCode()
         {
             UnityEngine.TestTools.LogAssert.ignoreFailingMessages = true;
             DazelCompiler dazelCompiler = new DazelCompiler(TestCode2_1, TestCode2_2);
